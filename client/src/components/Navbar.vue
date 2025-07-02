403cec647a0690ad16659e66c28d4a5e9f83c7f2
heather bickish <hbickish@gmail.com>
2025-07-02 15:44:16 -0600

    landing page

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { loadState, saveState } from '../utils/Store.js';
+// import { ref, watch } from 'vue';
+// import { loadState, saveState } from '../utils/Store.js';
 import Login from './Login.vue';
 
-const theme = ref(loadState('theme') || 'light')
+// const theme = ref(loadState('theme') || 'light')
 
-function toggleTheme() {
-  theme.value = theme.value == 'light' ? 'dark' : 'light'
-}
+// function toggleTheme() {
+//   theme.value = theme.value == 'light' ? 'dark' : 'light'
+// }
 
-watch(theme, () => {
-  document.documentElement.setAttribute('data-bs-theme', theme.value)
-  saveState('theme', theme.value)
-}, { immediate: true })
+// watch(theme, () => {
+//   document.documentElement.setAttribute('data-bs-theme', theme.value)
+//   saveState('theme', theme.value)
+// }, { immediate: true })
 
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md bg-codeworks border-bottom border-vue">
+  <nav class="navbar navbar-expand-md bg-dark border-bottom border-vue">
     <div class="container gap-2">
       <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light">
-        <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" />
-        <b class="fs-5">Vue Starter</b>
+        <!-- <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" /> -->
+        <b class="fs-4">Nampa Neighborhood Network</b>
       </RouterLink>
       <!-- collapse button -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
@@ -31,19 +31,19 @@ watch(theme, () => {
       <!-- collapsing menu -->
       <div class="collapse navbar-collapse " id="navbar-links">
         <ul class="navbar-nav">
-          <li>
+          <!-- <li>
             <RouterLink :to="{ name: 'About' }" class="btn text-green selectable">
               About
             </RouterLink>
-          </li>
+          </li> -->
         </ul>
         <!-- LOGIN COMPONENT HERE -->
         <div class="ms-auto">
-          <button class="btn text-light" @click="toggleTheme"
+          <!-- <button class="btn text-light" @click="toggleTheme"
             :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
             <i v-if="theme == 'dark'" class="mdi mdi-weather-sunny"></i>
             <i v-if="theme == 'light'" class="mdi mdi-weather-night"></i>
-          </button>
+          </button> -->
         </div>
         <Login />
       </div>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -21,15 +21,41 @@ async function getAllTasks() {
 </script>
 
 <template>
-  <section class="row mx-0">
-    <div class="col-md-5">
-      <div>
-        <h1>Hello neighbor!</h1>
-        <h3>Nampa's Way to Connect and Thrive.</h3>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="mt-5">
+          <h1>Hello neighbor!</h1>
+          <h3>Nampa's way to connect and thrive!</h3>
+          <div class="mt-3">
+            <p>The Nampa Neighborhood Network is your local hub for practical help and shared resources. Need a hand
+              with some housework, a minor home repair, or even borrowing a specific tool like a lawnmower or power
+              drill? This app connects you directly with neighbors who have the skills or equipment you need.
+              <br>
+              <br>
+              It's all about fostering a supportive Nampa community where reaching out for help or offering your unique
+              talents is simple, convenient, and truly local.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-7">
+        <div class="mt-5">
+          <img
+            src="https://images.unsplash.com/photo-1620841713108-18ad2b52d15c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="" class="hero">
+        </div>
       </div>
     </div>
-    <div class="col-md-5"></div>
   </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.hero {
+  height: 20rem;
+  width: 100%;
+  object-position: center;
+  object-fit: cover;
+  border-radius: 5px;
+}
+</style>
